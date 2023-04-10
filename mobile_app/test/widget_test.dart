5d0edf50375c43710b0a7ac13b908cdb8622a593
Damian Raczkowski <idontknow5325@gmail.com>
2023-04-10 11:28:50 +0200

    Add widget test for bottombar and login view
    Signed-off-by: Damian Raczkowski <idontknow5325@gmail.com>

--- mobile_app/test/widget_test.dart
+++ mobile_app/test/widget_test.dart
@@ -8,16 +8,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_app/main.dart';
+import 'package:mobile_app/screens/main_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Display login view and move user into another view',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    final barcodeButtonFinder = find.byKey(Key("ScannerButton"));
-    final testBarCodeFinder = find.byKey(Key("barcode"));
+    final loginButtonFinder = find.byKey(const Key("log-in-button"));
+    final imageFinder = find.byKey(const Key("image-box"));
     // Verify that our counter starts at 0.
-    expect(barcodeButtonFinder, findsOneWidget);
-    expect(testBarCodeFinder, findsOneWidget);
+    expect(loginButtonFinder, findsOneWidget);
+    expect(imageFinder, findsOneWidget);
+
+    await tester.tap(loginButtonFinder);
+  });
+  testWidgets("Ensure that user can move between pages after log in",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LoggedInView()));
+
+    final bottomNavigationBar = find.byKey(const Key("navigation-bar"));
+    expect(bottomNavigationBar, findsOneWidget);
+
+    await tester.tap(find.byIcon( Icons.center_focus_strong_outlined));
+    expect(find.text("Scanner"), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.settings));
+    expect(find.text("Settings"), findsWidgets);
+
+    await tester.tap(find.byIcon(Icons.home));
+    expect(find.text("Inventories"), findsOneWidget);
   });
 }
