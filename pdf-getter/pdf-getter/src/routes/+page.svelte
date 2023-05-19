47a668d2cc07869cef9fb98ccf64e771dec4ae11
Joe676 <bossowski92@gmail.com>
2023-05-19 18:40:17 +0200

    initial sveltekit project with google authorization

---
+++ pdf-getter/pdf-getter/src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { authHandlers, authStore } from "../stores/authStore";
+
+  // import {Auth} from "$lib/components/Auth.svelte";
+  async function handleLogin() {
+    try {
+      await authHandlers.signin();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleLogout() {
+    try {
+      await authHandlers.logout();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+</script>
+
+<form>
+
+  {#if $authStore.currentUser}
+    {$authStore.currentUser.displayName}
+    <button on:click={handleLogout}>
+      Logout with Google...
+    </button>
+  {:else}
+    Not logged in
+    <button on:click={handleLogin}>
+      Login with Google...
+    </button>
+  {/if}
+</form>
+<!-- <Auth/> -->
---
+++ pdf-getter/pdf-getter/src/stores/authStore.js
@@ -0,0 +1,29 @@
+// import {} from "firebase/auth";
+import {writable} from "svelte/store";
+import { auth, googleProvider } from "../lib/firebase/firebase.client";
+import { signInWithPopup, signOut } from "firebase/auth";
+
+export const authStore = writable({
+  isLoading: true,
+  currentUser: null
+});
+
+export const authHandlers = {
+  logout: async () => {
+    await signOut(auth);
+  },
+  signin: async () => {
+    await signInWithPopup(auth, googleProvider);
+    // .then(function(result) {
+    //   // var token = result.credential.accessToken;
+    //   var firebaseuser = result.user;
+
+    //   if(firebaseuser) {
+    //       authStore.update((curr)=>{
+    //         return {...curr, isLoading: false, currentUser: firebaseuser};
+    //       });
+    //   }
+    //   // ...
+    // });
+  }
+};
